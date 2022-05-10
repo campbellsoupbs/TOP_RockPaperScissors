d5647f1cbdbd841ed7695e48cae0ab0b59241b80
Brian Campbell <campbellsoupbs@gmail.com>
2022-05-09 23:27:53 -0400

    Create playRound function

--- script.js
+++ script.js
@@ -1,5 +1,3 @@
-console.log("Hello World!")
-
 function computerPlay() {
     let computerTurn = Math.floor(Math.random()*100)
     if (computerTurn < 33) {
@@ -11,4 +9,41 @@ function computerPlay() {
     }
 }
 
-console.log(computerPlay())
+function playRound() {
+    let playerTurn = prompt("Enter your choice: ");
+    playerTurn = playerTurn.toLowerCase();
+    let computerTurn = computerPlay();
+    console.log("Comp: "+computerTurn)
+    console.log("Player: "+playerTurn)
+    switch (playerTurn) {
+        case "rock":
+            if (computerTurn === "scissors") {
+                return "You win! Rock beats scissors"
+            } else if (computerTurn === "paper") {
+                return "You lose! Paper beats rock"
+            } else {
+                return "Tie!"
+            };
+            break;
+        case "paper":
+            if (computerTurn === "scissors") {
+                return "You lose! Scissors beats paper"
+            } else if (computerTurn === "rock") {
+                return "You win! Paper beats rock"
+            } else {
+                return "Tie!"
+            };
+            break;
+            
+        case "scissors":
+            if (computerTurn === "paper") {
+                return "You win! Scissors beats paper"
+            } else if (computerTurn === "rock") {
+                return "You lose! Rock beats scissors"
+            } else {
+                return "Tie!"
+            };
+            break;                          
+    }
+}
+console.log(playRound())
