8ee60ca4987dfc0cf59c635b9b9dc680d44405a3
Brian Campbell <campbellsoupbs@gmail.com>
2022-05-31 23:56:16 -0400

    Edit .js to play round of rps and log outcome when pic clicked

--- script.js
+++ script.js
@@ -1,51 +1,50 @@
-// function computerPlay() {
-//     let computerTurn = Math.floor(Math.random()*100)
-//     if (computerTurn < 33) {
-//         return "rock"
-//     } else if (computerTurn < 66 && computerTurn > 33) {
-//         return "paper"
-//     } else {
-//         return "scissors"
-//     }
-// }
+function computerPlay() {
+    let computerTurn = Math.floor(Math.random()*100)
+    if (computerTurn < 33) {
+        return "rock"
+    } else if (computerTurn < 66 && computerTurn > 33) {
+        return "paper"
+    } else {
+        return "scissors"
+    }
+}
 
-// function playRound() {
-//     let playerTurn = prompt("Enter your choice: ");
-//     playerTurn = playerTurn.toLowerCase();
-//     let computerTurn = computerPlay();
-//     console.log("Computer: "+computerTurn)
-//     console.log("Player: "+playerTurn)
-//     switch (playerTurn) {
-//         case "rock":
-//             if (computerTurn === "scissors") {
-//                 return "You win! Rock beats scissors"
-//             } else if (computerTurn === "paper") {
-//                 return "You lose! Paper beats rock"
-//             } else {
-//                 return "Tie!"
-//             };
-//             break;
-//         case "paper":
-//             if (computerTurn === "scissors") {
-//                 return "You lose! Scissors beats paper"
-//             } else if (computerTurn === "rock") {
-//                 return "You win! Paper beats rock"
-//             } else {
-//                 return "Tie!"
-//             };
-//             break;
+function playRound(player) {
+    let playerTurn = player;
+    let computerTurn = computerPlay();
+    console.log("Computer: "+computerTurn)
+    console.log("Player: "+playerTurn)
+    switch (playerTurn) {
+        case "rock":
+            if (computerTurn === "scissors") {
+                return "You win! Rock beats scissors"
+            } else if (computerTurn === "paper") {
+                return "You lose! Paper beats rock"
+            } else {
+                return "Tie!"
+            };
+            break;
+        case "paper":
+            if (computerTurn === "scissors") {
+                return "You lose! Scissors beats paper"
+            } else if (computerTurn === "rock") {
+                return "You win! Paper beats rock"
+            } else {
+                return "Tie!"
+            };
+            break;
             
-//         case "scissors":
-//             if (computerTurn === "paper") {
-//                 return "You win! Scissors beats paper"
-//             } else if (computerTurn === "rock") {
-//                 return "You lose! Rock beats scissors"
-//             } else {
-//                 return "Tie!"
-//             };
-//             break;                          
-//     }
-// }
+        case "scissors":
+            if (computerTurn === "paper") {
+                return "You win! Scissors beats paper"
+            } else if (computerTurn === "rock") {
+                return "You lose! Rock beats scissors"
+            } else {
+                return "Tie!"
+            };
+            break;                          
+    }
+}
 
 // function game() {
 //     let playerScore = 0;
@@ -86,13 +85,16 @@ scissors.addEventListener('click', pickScissors);
 
 //Functions
 function pickRock(e){
-    console.log('Rock!')
+    let player = 'rock';
+    console.log(playRound(player))
 }
 
 function pickPaper(e){
-    console.log('Paper!')
+    let player = 'paper';
+    console.log(playRound(player))
 }
 
 function pickScissors(e){
-    console.log('Scissors!')
+    let player = 'scissors';
+    console.log(playRound(player))
 }
